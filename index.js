702e923cd14fe0be2f4f42b60e526f29f5cd2c97
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-21 03:14:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var recipes=
 {
   
 }
- function updateObjectWithKeyAndValue(object,key,value){
+ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
    
    object[key]=value
    return object
  }
  
- 
+ function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
+}
